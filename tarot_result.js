a211a19f24f02e176620f953f35792f20ee5d12e
rubypowerr <113876450+rubypowerr@users.noreply.github.com>
2022-10-21 11:45:39 +1100

    Update tarot_result.js

--- tarot_result.js
+++ tarot_result.js
@@ -1,6 +1,6 @@
 //variable of total number of images without having to manually change the numbers
 var totImages=78;
-
+var state="read";
 function preload(){
 //display of the images and its range from 1-78
   let rand1=int(random(1,totImages))
@@ -25,6 +25,11 @@ function preload(){
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+function printReady(){
+  resizeCanvas(384, 2000);
+  pixelDensity(1);
+  state="print";
+}
 
 function setup() {
 //basic page set up and where the text will look and be
@@ -42,8 +47,9 @@ function setup() {
   button.mousePressed(home);
   
 //print button next to the title
-  button2 = createButton('receipt' + '?');
-  button2.position(100,20);
+button2 = createButton('receipt' + '?');
+  button2.position(100, 20);
+  button2.mousePressed(printReady);
  
 //change image sizes
   img.resize (200,300);
@@ -55,12 +61,28 @@ function setup() {
 
 function draw() {
 //title of the screen
-  text('You have been read', width/2, 100);
-
   
+
+ if (state=="read"){ 
+   text('You have been read', width/2, 100);
 //the images and their placement on the screen
 image(img,width/6, height/3);
     image(img2,width/2.4, height/3);
       image(img3,width-width/3, height/3);
-        
+ } 
+  else {
+   // text('Tarot Booth', width/2, 20);
+    background(255);
+    image(img,img.width-width/2, 40);
+    image(img2,img2.width-width/2, img.height+80);
+      image(img3,img3.width-width/2, img.height+img2.height+120);
+    button.remove();
+    button2.remove();
+  }
+ 
+}
+function keyPressed(){
+  if (key==" "){
+    saveFrames('tarotbooth','jpg',1,1);
+  }
 }
